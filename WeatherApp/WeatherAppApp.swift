c8e3b6b21ae301cfe41b066003890dddcf277845
faiyamm <sofia.almaraz@uabc.edu.mx>
2026-02-20 21:04:55 -0800

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Fai on 20/02/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
